365f58d7fc2c4bd6f37125e0688e03e209b0cee2
junil-hwang <tjsdlf4261@naver.com>
2020-04-30 00:36:15 +0900

    "commit.sh 수정"

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ set -e
 yarn docs:build
 cd .vuepress/dist
 git clone -b gh-pages https://github.com/DKU-STUDY/DKU-STUDY.github.io
-cp -rf TIL/.git ./.git
-rm -rf TIL
+cp -rf DKU-STUDY.github.io/.git ./.git
+rm -rf DKU-STUDY.github.io
 git add .
 git commit -m '$1'
 git push origin gh-pages
