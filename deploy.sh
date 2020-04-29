a1acb186a973b2abf83cab475cd26e731f395f04
junil-hwang <tjsdlf4261@naver.com>
2020-04-30 00:39:40 +0900

    "commit.sh 수정"

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git clone -b gh-pages https://github.com/DKU-STUDY/DKU-STUDY.github.io
 cp -rf DKU-STUDY.github.io/.git ./.git
 rm -rf DKU-STUDY.github.io
 git add .
-git commit -m '$1'
+git commit -m "$*"
 git push origin gh-pages
 cd -
