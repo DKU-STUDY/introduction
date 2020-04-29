c1c1f30ad3d5eb95fad87338ad71c1e3e285dd29
junil-hwang <tjsdlf4261@naver.com>
2020-04-30 00:59:54 +0900

    fix: shell script 수정

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 yarn docs:build
 cd .vuepress/dist
-git clone -b gh-pages https://github.com/DKU-STUDY/DKU-STUDY.github.io
+git clone -b master https://github.com/DKU-STUDY/DKU-STUDY.github.io
 cp -rf DKU-STUDY.github.io/.git ./.git
 rm -rf DKU-STUDY.github.io
 git add .
-git commit -m "$*"
-git push origin gh-pages
+git commit -m $*
+git push origin +master
 cd -
