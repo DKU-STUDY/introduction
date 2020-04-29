31b6a4137305ec5e4054c327fcdd5c4541efcd80
junil-hwang <tjsdlf4261@naver.com>
2020-04-30 00:33:50 +0900

    "config 설정 완료"

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'DKU-STUDY', // 사이트 타이틀
+  description: '단국대학교 알고리즘 스터디',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' }
+    ],
+    sidebar: 'auto' // h1~h6 같은 heading tag를 기준으로 sidebar를 만들어줌
+  }
+}
