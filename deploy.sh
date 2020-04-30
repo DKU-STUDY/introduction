0a2c1894a4cf5e2f3ae2b342c2a1c7bde4d7a6c2
junilHwang <tjsdlf4261@naver.com>
2020-04-30 14:15:34 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,6 @@ git clone -b master https://github.com/DKU-STUDY/DKU-STUDY.github.io
 cp -rf DKU-STUDY.github.io/.git ./.git
 rm -rf DKU-STUDY.github.io
 git add .
-git commit -m $*
+git commit -m "$*"
 git push origin +master
 cd -
