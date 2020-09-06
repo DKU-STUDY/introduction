e103a746955dd772e6138c78196a1b289a4f4a1d
eyabc <bey4314@naver.com>
2020-09-06 23:45:49 +0900

    2020 09 07

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -4,9 +4,14 @@ module.exports = {
     '/':
         [
             { title: '스터디 개요', path: '/' },
-            { title: '스터디 입과 절차', path: './Beginner' },
+            { title: '그룹 입과 절차', path: './Beginner' },
             { title: '디스코드 사용 방법', path: './Discord' },
-            utils.getSideBar({ title: '스터디 활동', folder: '/Activity' }) ,
+            { title: '스터디 활동', path: './Activity' },
+            { title: 'Algorithm 스터디', path: './Algorithm' },
+            { title: 'JavaScript 스터디', path: './JavaScript' },
+            { title: '이달의 책 읽기', path: './BookOfThisMonth' },
+            utils.getSideBar({ title: '프로젝트', folder: '/Project' }) ,
+
         ],
 }
 ;
---
+++ JavaScript.md
@@ -0,0 +1,4 @@
+# JavaScript 스터디
+
+[스터디에 대한 상세 활동과 규칙](https://eyabc.github.io/Doc/dev/dku-javascript-study/%EA%B2%B0%EA%B3%BC%EB%AC%BC.html#%EA%B7%9C%EC%B9%99)
+
